8de35de1b66700d02aeb8e771cf7bad976bfd083
Adamwojty <adam12573@gmail.com>
2020-09-07 20:58:17 +0200

    Add mobile version

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 import { Provider } from "react-redux";
 import Store from "./config/redux";
 import App from "./App";
+const backendOptions = {
+  enableMouseEvents: true,
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={TouchBackend} options={backendOptions}>
         <App />
       </DndProvider>
     </Provider>
